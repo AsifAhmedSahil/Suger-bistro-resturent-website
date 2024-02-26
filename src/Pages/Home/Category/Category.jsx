8bc314c2ee4cd34cfb04d2984548ddf7e5ec78f6
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-02-26 16:16:37 +0600

    swiper js done

---
+++ src/Pages/Home/Category/Category.jsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+import slide1 from "../../../assets/home/slide1.jpg"
+import slide2 from "../../../assets/home/slide2.jpg"
+import slide3 from "../../../assets/home/slide3.jpg"
+import slide4 from "../../../assets/home/slide4.jpg"
+import slide5 from "../../../assets/home/slide5.jpg"
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const Category = () => {
+  return (
+    <div>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mt-10 mb-20 "
+      >
+        <SwiperSlide>
+            <img src={slide1} alt="" />
+            <h3 className='uppercase text-4xl text-white  text-center -mt-16'>Salad</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide2} alt="" />
+            <h3 className='uppercase text-4xl text-white  text-center -mt-16'>pizza</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide3} alt="" />
+            <h3 className='uppercase text-4xl text-white  text-center -mt-16'>Soups</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide4} alt="" />
+            <h3 className='uppercase text-4xl text-white  text-center -mt-16'>Desurts</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+            <img src={slide5} alt="" />
+            <h3 className='uppercase text-4xl text-white  text-center -mt-16'>Salad</h3>
+        </SwiperSlide>
+        
+        
+      </Swiper>
+    </div>
+  )
+}
+
+export default Category
