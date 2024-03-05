3f25bce0e151e445201bc2762f0a09d0d44dfcbf
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-05 11:03:56 +0600

    order food route setup

---
+++ src/Pages/Order/Order/Order.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cover from '../../Shared/Cover'
+import orderCoverImg from "../../../assets/shop/banner2.jpg"
+
+const Order = () => {
+  return (
+    <div>
+      <Cover img={orderCoverImg} title="Order Food"></Cover>
+    </div>
+  )
+}
+
+export default Order
