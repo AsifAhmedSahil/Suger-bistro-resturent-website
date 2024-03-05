445f0db95d2f0ad4685815c50ec793799190e82d
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-05 11:16:22 +0600

    Food Order Page With Tabs

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -4,9 +4,16 @@ import orderCoverImg from "../../../assets/shop/banner2.jpg";
 
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import useMenu from "../../../hooks/useMenu";
 
 const Order = () => {
     const [tabIndex,setTabIndex] = useState(0)
+    const [menu] = useMenu()
+    const dessert = menu.filter(item => item.category === 'dessert')
+    const soup = menu.filter(item => item.category === 'soup')
+    const salad = menu.filter(item => item.category === 'salad')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const offered = menu.filter(item => item.category === 'offered')
   return (
     <div>
       <Cover img={orderCoverImg} title="Order Food"></Cover>
