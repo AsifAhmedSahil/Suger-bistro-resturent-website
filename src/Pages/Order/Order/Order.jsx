e9fd43e95e9e95d3f69f3b838ccac1d8cfdbed44
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-05 16:21:07 +0600

    order page data move on selected done

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -8,11 +8,15 @@ import useMenu from "../../../hooks/useMenu";
 import FoodOrder from "../../../Componets/FoodOrder/FoodOrder";
 import OrderTab from "../OrderTab/OrderTab";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Order = () => {
-    const [tabIndex,setTabIndex] = useState(0)
-    const [menu] = useMenu()
+    const categories = ['Salad','Pizza','Soup','Dessert','Drinks']
     const {category} = useParams();
+    const initialIndex = categories.indexOf(category)
+    const [tabIndex,setTabIndex] = useState(initialIndex)
+    const [menu] = useMenu()
+    
     const dessert = menu.filter(item => item.category === 'dessert')
     const soup = menu.filter(item => item.category === 'soup')
     const salad = menu.filter(item => item.category === 'salad')
@@ -20,6 +24,9 @@ const Order = () => {
     const drinks = menu.filter(item => item.category === 'drinks')
   return (
     <div>
+        <Helmet>
+            <title>Suger Bistro | Order Menu</title>
+        </Helmet>
       <Cover img={orderCoverImg} title="Order Food"></Cover>
 
       <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
