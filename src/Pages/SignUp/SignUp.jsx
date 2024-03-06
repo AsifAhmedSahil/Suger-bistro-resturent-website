804d24d4b3f35acc5dc566f08fd9ac0f1bd31e20
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-06 12:02:02 +0600

    logout setup

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -10,91 +10,92 @@ const SignUp = () => {
     watch,
     formState: { errors },
   } = useForm();
-  const {createUser} = useContext(AuthContext)
-
+  const { createUser } = useContext(AuthContext);
 
   const onSubmit = (data) => {
-    console.log(data)
-    createUser(data.email,data.password)
-    .then(result =>{
-      const loggedUser = result.user
-      console.log(loggedUser)
-    })
-  }
-
-
-  
-
+    console.log(data);
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
+      console.log(loggedUser);
+    });
+  };
 
   return (
     <>
-    <Helmet>
-            <title>Suger Bistro | Sign Up</title>
-        </Helmet>
+      <Helmet>
+        <title>Suger Bistro | Sign Up</title>
+      </Helmet>
       <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Sign Up</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                {...register("name",{ required: true })}
-                placeholder="name"
-                className="input input-bordered"
-                // required
-              />
-              {errors.name && <span className="text-red-600 mt-2">*Name is required</span>}
-            </div>
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Sign Up</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  {...register("name", { required: true })}
+                  placeholder="name"
+                  className="input input-bordered"
+                  // required
+                />
+                {errors.name && (
+                  <span className="text-red-600 mt-2">*Name is required</span>
+                )}
+              </div>
 
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                {...register("email",{required: true})}
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
-              {errors.email && <span className="text-red-600 mt-2">*Email is required</span>}
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                {...register("password")}
-                placeholder="password"
-                className="input input-bordered"
-                required
-              />
-              <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <input className="btn btn-primary" type="submit" value="Sign Up" />
-              
-            </div>
-          </form>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  {...register("email", { required: true })}
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+                {errors.email && (
+                  <span className="text-red-600 mt-2">*Email is required</span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  {...register("password")}
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Sign Up"
+                />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
