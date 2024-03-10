19acb461baa222612255dfd12da349abf21e8c57
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-10 13:04:12 +0600

    401 and 403 setup successfully and move the user to the login page

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,15 @@
 import axios from 'axios'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import useAuth from './useAuth'
 
 const axiosSecure = axios.create({
     baseURL: "http://localhost:3000"
 })
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate()
+  const {logOut} = useAuth()
   axiosSecure.interceptors.request.use(function(config){
     const token = localStorage.getItem('access-token')
     config.headers.authorization = `Bearer ${token}`
@@ -17,8 +21,14 @@ const useAxiosSecure = () => {
 // axiosSecure 401 & 403 status
 axiosSecure.interceptors.response.use(function(response){
   return response;
-},(error) =>{
+},async (error) =>{
   const status = error.response.status;
+  console.log("status error in the interceptor",status)
+  if(status === 401 || status === 403){
+    await logOut();
+    navigate("/login")
+
+  }
   return Promise.reject(error);
 })
   
