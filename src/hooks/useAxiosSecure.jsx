86d8620da87c3c51139e4c9d5bea3dbe984d9c6a
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-10 12:55:47 +0600

    Verify Token And Axios Interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -6,6 +6,22 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+  axiosSecure.interceptors.request.use(function(config){
+    const token = localStorage.getItem('access-token')
+    config.headers.authorization = `Bearer ${token}`
+    return config
+  },function(error){
+    return Promise.reject(error);
+});
+
+// axiosSecure 401 & 403 status
+axiosSecure.interceptors.response.use(function(response){
+  return response;
+},(error) =>{
+  const status = error.response.status;
+  return Promise.reject(error);
+})
+  
   return axiosSecure
 }
 
