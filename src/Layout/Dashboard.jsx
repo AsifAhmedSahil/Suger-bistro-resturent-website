f447b79d6ef16b14754c36c8bed02b2955bed18c
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-10 21:49:42 +0600

    jwt problem

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -15,11 +15,12 @@ import {
 import { MdOutlineMenu } from "react-icons/md";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
+import useAdmin from "../hooks/useAdmin";
 
 const Dashboard = () => {
   const [cart] = useCart();
 
-  const isAdmin = true;
+  const [isAdmin] = useAdmin();
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-orange-500">
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -23,7 +23,7 @@ axiosSecure.interceptors.response.use(function(response){
   return response;
 },async (error) =>{
   const status = error.response.status;
-  console.log("status error in the interceptor",status)
+  // console.log("status error in the interceptor",status)
   if(status === 401 || status === 403){
     await logOut();
     navigate("/login")
