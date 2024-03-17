f4da84c333464203b4941819502d702194f7a2d4
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-18 00:55:49 +0600

    Make User Api Secure Using Verify Admin

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -1,46 +1,21 @@
-// import React from 'react'
-// import useAuth from './useAuth'
-// import useAxiosSecure from './useAxiosSecure'
-// import { useQuery } from '@tanstack/react-query'
 
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 
-// const useAdmin = () => {
-//     const {user} = useAuth()
-//     const axiosSecure = useAxiosSecure()
-
-//     const {data: isAdmin} = useQuery({
-        
-//         queryKey: [user?.email, 'isAdmin'],
-        
-//         queryFn: async() =>{
-//             console.log(user?.email)
-//             const res = await axiosSecure.get(`/users/admin/:${user.email}`);
-//             console.log(res.data)
-//             return res.data?.admin;
-//         }
-//     })
-//     return [isAdmin]
-// }
-
-// export default useAdmin
-
 
 const useAdmin = () =>{
     const {user} = useAuth()
     const axiosSecure = useAxiosSecure()
-    const {data: isAdmin} = useQuery({
+    const {data: isAdmin,isPending:isAdminLoading} = useQuery({
         queryKey:[user?.email,'isAdmin'],
         queryFn: async () =>{
             const res = await axiosSecure.get(`/users/admin/${user.email}`)
             console.log(res.data)
             return res.data?.admin
         }
-
     })
-    return [isAdmin]
+    return [isAdmin,isAdminLoading]
 
 }
 
