d464ae1a3848f6e440d562ee9f3ea11100ee099b
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-22 00:24:00 +0600

    issue fetch payment data

--- src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
+import useAuth from '../../../hooks/useAuth'
+import useAxiosSecure from '../../../hooks/useAxiosSecure'
+import { useQuery } from '@tanstack/react-query'
 
 const PaymentHistory = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    const { user } = useAuth()
+    console.log(user.email)
+    const axiosSecure = useAxiosSecure()
+
+
+    const {data: payments=[]} = useQuery({
+        queryKey:['payments',user.email],
+        queryFn: async() =>{
+            const res = await axiosSecure.get(`/payments/${user.email}`)
+            return res.data;
+        }
+    })
+
+    
+
+    
+
+    return (
+        <div>
+            <h3 className='text-3xl'>Total Payments: </h3>
+        </div>
+    );
 }
 
-export default PaymentHistory
+export default PaymentHistory;
+
