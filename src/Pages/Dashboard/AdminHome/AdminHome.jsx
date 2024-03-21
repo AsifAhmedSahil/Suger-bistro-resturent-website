8d51921eb16d9e3ae7025ecdc36e59eeb39f1398
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-22 04:17:11 +0600

    error for states

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
-import useAuth from '../../../hooks/useAuth'
+import React from "react";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const AdminHome = () => {
-    const {user} = useAuth()
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
   return (
     <div>
-        <h3>Hi, Welcome</h3>
-        {
-            user?.displayName ? user.displayName : 'back'
-        }
-      
-    </div>
-  )
-}
+      <h3>Hi, Welcome</h3>
+      {user?.displayName ? user.displayName : "back"}
 
-export default AdminHome
+      <div className="stats shadow">
+        <div className="stat place-items-center">
+          <div className="stat-title">revenue</div>
+          <div className="stat-value">{stats.revenue}</div>
+          <div className="stat-desc">From January 1st to February 1st</div>
+        </div>
+
+        <div className="stat place-items-center">
+          <div className="stat-title">Users</div>
+          <div className="stat-value text-secondary">4,200</div>
+          <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
+        </div>
+
+        <div className="stat place-items-center">
+          <div className="stat-title">New Registers</div>
+          <div className="stat-value">1,200</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminHome;
--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -6,6 +6,7 @@ import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin = () =>{
     const {user,loading} = useAuth()
+    // const {user} = useAuth()
     const axiosSecure = useAxiosSecure()
     const {data: isAdmin,isPending:isAdminLoading} = useQuery({
         queryKey:[user?.email,'isAdmin'],
