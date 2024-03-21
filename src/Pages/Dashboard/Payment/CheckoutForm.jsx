c7db9c1cf6c68b711307af0391f3b5f074a34ec9
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-21 19:08:11 +0600

    Use Confirm Card Payment To Process Payment

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -2,15 +2,19 @@ import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import React, { useEffect, useState } from "react";
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import useCart from '../../../hooks/useCart';
+import useAuth from "../../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 
 const CheckoutForm = () => {
   const [error,setError] = useState()
   const [clientSecret,setClientSecret] = useState()
+  const [transactionId,setTransectionId] = useState()
   const stripe = useStripe();
   const elements = useElements();
   const axiosSecure = useAxiosSecure()
   const [cart] = useCart()
+  const {user} = useAuth()
   
   const totalPrice = cart.reduce((total,item)=> total+item.price,0)
   console.log("total price under checkout form",totalPrice);
@@ -52,6 +56,33 @@ const CheckoutForm = () => {
       console.log('[PaymentMethod]', paymentMethod);
       setError("")
     }
+
+    const {paymentIntent,error: confirmError} = await stripe.confirmCardPayment(clientSecret,{
+      payment_method:{
+        card: card,
+        billing_details: {
+          email: user?.email || 'anonymous',
+          name: user?.displayName || 'anonymous'
+        }
+      }
+    })
+
+    if(confirmError){
+      console.log("confirm error" )
+    }
+    else{
+      console.log('payment intent',paymentIntent)
+      if(paymentIntent.status === 'succeeded'){
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your Payment has been completed",
+          showConfirmButton: false,
+          timer: 1000
+        });
+        setTransectionId(paymentIntent.id);
+      }
+    }
   };
 
   return (
@@ -73,13 +104,16 @@ const CheckoutForm = () => {
             },
           }}
         />
-        <button className="btn rounded-lg bg-black text-white hover:text-black" type="submit" disabled={!stripe || !clientSecret}>
+        <button className="btn mt-8 rounded-lg bg-black text-white hover:text-black" type="submit" disabled={!stripe || !clientSecret}>
         {/* <button className="btn rounded-lg bg-black text-white hover:text-black" type="submit" > */}
           Pay
         </button>
       </form>
 
       <p className="text-red-600">{error}</p>
+      {
+        transactionId && <p className="text-green-600 mt-8">Your Transaction Id is: {transactionId}</p>
+      }
     </div>
   );
 };
