9dceec5a0970fed951e6ba1dce7e3131b01cd09b
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-03-22 15:12:35 +0600

    Load Data And Display Stats On The Admin Home

--- src/Pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/AdminHome/AdminHome.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import { FaBook, FaBorderAll, FaDollarSign, FaUsers } from "react-icons/fa";
 
 const AdminHome = () => {
   const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
-  const { data: stats } = useQuery({
+  const { data: stats = [] } = useQuery({
     queryKey: ["admin-stats"],
     queryFn: async () => {
       const res = await axiosSecure.get("/admin-stats");
@@ -19,24 +20,43 @@ const AdminHome = () => {
       <h3>Hi, Welcome</h3>
       {user?.displayName ? user.displayName : "back"}
 
-      <div className="stats shadow">
-        <div className="stat place-items-center">
-          <div className="stat-title">revenue</div>
+      <div className="mt-24 ">
+      <div className="stats shadow ">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-3xl text-red-600 "></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
           <div className="stat-value">{stats.revenue}</div>
-          <div className="stat-desc">From January 1st to February 1st</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
         </div>
 
-        <div className="stat place-items-center">
-          <div className="stat-title">Users</div>
-          <div className="stat-value text-secondary">4,200</div>
-          <div className="stat-desc text-secondary">↗︎ 40 (2%)</div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers className="text-3xl text-red-600 "></FaUsers>
+          </div>
+          <div className="stat-title"> Users</div>
+          <div className="stat-value">{stats.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBook className="text-3xl text-red-600 "></FaBook>
+          </div>
+          <div className="stat-title"> Menu Items</div>
+          <div className="stat-value">{stats.menuItems}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
         </div>
 
-        <div className="stat place-items-center">
-          <div className="stat-title">New Registers</div>
-          <div className="stat-value">1,200</div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+          <FaBorderAll className="text-3xl text-red-600 " />
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats.orders}</div>
           <div className="stat-desc">↘︎ 90 (14%)</div>
         </div>
+      </div>
       </div>
     </div>
   );
